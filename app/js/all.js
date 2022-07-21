1b57c861f92f5c6784ac71e8aa59d4ca42c601c9
b9111426 <b9111426@gmail.coom>
2022-07-21 18:49:38 +0800

    frist update

---
+++ app/js/all.js
@@ -0,0 +1,7 @@
+import { deviceName, mobilesOnSale, offers, logPrice, logDeviceName } from  './test.js'
+
+console.log(deviceName)
+console.log(mobilesOnSale)
+console.log(offers)
+console.log(logPrice(600))
+console.log(logDeviceName('galaxy'))
---
+++ app/js/test.js
@@ -0,0 +1,15 @@
+
+export const deviceName = 'iPhone'
+export const mobilesOnSale = ['Samsung', 'Apple', 'Huawei']
+
+export const offers = {
+	priceCurrency: 'TWD',
+	price: '26,900',
+}
+export const logPrice = (price) => {
+	console.log('price: ', price)
+}
+
+export function logDeviceName(deviceName) {
+	console.log(deviceName)
+}
